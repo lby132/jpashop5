fe59a9b703b5264d3d5b977a3820842c1279dea1
lby132 <quddbscjswow@naver.com>
2023-06-04 14:13:48 +0900

    Initial commit

---
+++ src/main/java/jpabook/jpashop5/Jpashop5Application.java
@@ -0,0 +1,13 @@
+package jpabook.jpashop5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jpashop5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jpashop5Application.class, args);
+    }
+
+}
---
+++ src/test/java/jpabook/jpashop5/Jpashop5ApplicationTests.java
@@ -0,0 +1,13 @@
+package jpabook.jpashop5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jpashop5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
